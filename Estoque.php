1ce221ceeebcb3751a53341efee1fa4fdfd1614e
Welington <wzaunir@gmail.com>
2016-09-22 21:51:49 -0300

    Aula 4 - Classes e Design Patters

--- Estoque.php
+++ Estoque.php
@@ -7,13 +7,22 @@
 class Estoque 
 {
     private $produtos = array();
+    private $pdo;
+    
+    public function __construct(PDO $db) {
+        $this->pdo = $db;
+    }
+
     
     /**
      * Adiciona um produto a tabela
      * @param int $produto
      * @param int $quantidade
      */
-    public function addProduto($produto,$quantidade){
+    public function addProduto(Produto $produto,$quantidade){
+        
+        $this->produtos[$produto->getCodigo()]['quantidade'] = $quantidade;
+        $this->produtos[$produto->getCodigo()]['produto'] = $produto;
         
     }
     
@@ -22,7 +31,7 @@ class Estoque
      * @param int $produto
      * @param int $quantidade
      */
-    public function remProduto($produto,$quantidade){
+    public function remProduto(Produto $produto,$quantidade){
         
        
     }
@@ -31,6 +40,9 @@ class Estoque
      * @return array Produtos
      */
     public function listarTudo(){
+        $sql = 'SELECT * FROM produto';
+        $retorno = $this->pdo->query($sql);
+        return $retorno->fetchAll(PDO::FETCH_ASSOC);
         
     }
     
@@ -40,6 +52,13 @@ class Estoque
      * @return Produto
      */
     public function listaProduto($codigo){
+        
+    }
+    
+    /**
+     * Sincroniza o objeto com o banco de dados
+     */
+    public function sinc(){
         
     }
     
--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
 
 require_once 'Produto.php';
+require_once 'Estoque.php';
 
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=Estoque','root','elaborata');
 $produto = new Produto();
+$produto->setCodigo(999991)
+        ->setNome('Camiseta Polo')
+        ->setTamanho('G')
+        ->setValor(75.80)
+        ->getModelo('Basico');
 
-var_dump($produto);
+$produto2 = new Produto();
+$produto2->setCodigo(999992)
+        ->setNome('Calcinha')
+        ->setTamanho('G')
+        ->setValor(5.80)
+        ->getModelo('Basica');
+
+
+$estoque = new Estoque($pdo);
+$estoque->addProduto($produto, 5);
+$estoque->addProduto($produto2, 10);
+$estoque->listarTudo();
+
+var_dump($estoque);
